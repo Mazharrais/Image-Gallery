8e0aab4b448f3b9ae86d1e2879f7defa00868f73
Mazharrais <mazharmajo72@gmail.com>
2024-08-01 12:08:02 +0500

    add code

---
+++ app.js
@@ -0,0 +1,9 @@
+
+let scrollContainer = document.querySelectorAll(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+})
