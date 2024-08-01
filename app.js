3887054ae1f390bd5ac8932f83c6a609d0f698d6
Mazharrais <mazharmajo72@gmail.com>
2024-08-01 12:28:37 +0500

    add code

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 
-let scrollContainer = document.querySelectorAll(".gallery");
+let scrollContainer = document.querySelector(".gallery");
 let backBtn = document.getElementById("backBtn");
 let nextBtn = document.getElementById("nextBtn");
 
 scrollContainer.addEventListener("wheel", (evt) => {
     evt.preventDefault();
     scrollContainer.scrollLeft += evt.deltaY;
-})
+    scrollContainer.style.scrollBehavior = "auto";
+
+});
+
+// nextBtn.addEventListener("event", () => {
+//     scrollContainer.style.scrollBehavior = "smooth";
+//     scrollContainer.scrollLeft += 900;
+// });
+
+// backBtn.addEventListener("event", () => {
+//     scrollContainer.style.scrollBehavior = "smooth";
+//     scrollContainer.scrollLeft -= 900;
+// });
+
+function nextBtn2(){
+    scrollContainer.style.scrollBehavior = "smooth";
+       scrollContainer.scrollLeft += 900;
+}
+
+function backBtn1(){
+    scrollContainer.style.scrollBehavior = "smooth";
+       scrollContainer.scrollLeft -= 900;
+}
+
